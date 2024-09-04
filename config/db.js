54141f880bcdf8944fabe6f8568fb5ad2662581d
rajeshk1909 <[rajeshkumark1909@gmail.com]>
2024-09-04 08:56:49 +0530

    update .env file

--- config/db.js
+++ config/db.js
@@ -1,17 +1,11 @@
 const mongoose = require("mongoose")
 
-require("dotenv").config()
-
-// Connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    console.log("Connected to MongoDB Atlas")
-  } catch (err) {
-    console.error("MongoDB connection error:", err)
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("MongoDB connected")
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message)
     process.exit(1)
   }
 }
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const path = require("path")
